6538b60c3fe6c48af55c3305c5587344f96b22f1
Ashley James <ashleytjames2110@gmail.com>
2025-02-20 12:18:06 +0000

    Navigation, animation controller, grabbing

---
+++ Assets/Ash/Scripts/NPC_Movement.cs
@@ -0,0 +1,266 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC_Movement : MonoBehaviour
+{
+    
+   //[SerializeField] Transform target;
+    NavMeshAgent agent;
+    Spawner spawner;
+
+    private List<List<Transform>> npcLocations = new List<List<Transform>>();
+    public HashSet<int> visitedLists = new HashSet<int>();
+    private int currentListIndex;
+    private Transform currentTarget;
+    private int spawnTimer;
+    
+    public bool grabbing = false;
+
+    public GameObject grabMarker;
+
+    Animator animator;
+    public Vector2 podiumDir;
+    private Vector2 direction;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    //private List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
+    
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        spawner = GameObject.FindWithTag("GameController").GetComponent<Spawner>();
+        npcLocations = new List<List<Transform>>(spawner.GetAllLocations());
+
+        grabMarker = gameObject.transform.Find("Grab").gameObject;
+        
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        MoveToPoint();
+    }
+    private void Update()
+    {
+        UpdateAnimator();
+        SortLayers();
+    }
+
+    private void MoveToPoint()
+    {
+        if (visitedLists.Count < 4)
+        {
+            do
+            {
+                currentListIndex = UnityEngine.Random.Range(0, npcLocations.Count);
+            } while (visitedLists.Contains(currentListIndex));
+
+            if (npcLocations[currentListIndex].Count > 0)
+            {
+                int randomLocationIndex = UnityEngine.Random.Range(0, npcLocations[currentListIndex].Count);
+                currentTarget = npcLocations[currentListIndex][randomLocationIndex];
+
+                agent.SetDestination(currentTarget.position);
+
+                npcLocations[currentListIndex].Remove(currentTarget);
+                spawner.RemoveLocation(currentTarget, currentListIndex);
+
+                StartCoroutine(CheckIfReachedDestination());
+            }
+            else
+            {
+                StartCoroutine(WaitAndRetry());
+            }
+        }
+        else
+        {
+            currentTarget = spawner.exitLocation;
+            agent.SetDestination(currentTarget.position);
+        }
+    }
+
+    private IEnumerator WaitAndRetry()
+    {
+        float waitTime = UnityEngine.Random.Range(2f, 8f);
+        yield return new WaitForSeconds(waitTime);
+        MoveToPoint();
+    }
+
+    
+    private IEnumerator CheckIfReachedDestination()
+    {
+        while (agent.pathPending || agent.remainingDistance > agent.stoppingDistance)
+        {
+            
+            yield return null;
+        }
+        
+
+        visitedLists.Add(currentListIndex);
+        StartCoroutine(WaitAtLocation());
+    }
+
+    private IEnumerator WaitAtLocation()
+    {
+        animator.SetBool("IsWalking", false);
+        animator.SetFloat("PodHor", podiumDir.x);
+        animator.SetFloat("PodVer", podiumDir.y);
+
+     
+
+        spawnTimer = UnityEngine.Random.Range(2, 8);
+        yield return new WaitForSeconds(spawnTimer);
+
+        if (UnityEngine.Random.value > 0.7f && UnityEngine.Random.value <= 1.0f)
+        {
+            StartCoroutine(Grabbing());
+        }
+        else
+        {
+            animator.SetBool("IsWalking", true);
+            spawner.AddLocation(currentTarget, currentListIndex);
+            MoveToPoint();
+        }
+    }
+
+    private IEnumerator Grabbing()
+    {
+        grabbing = true;
+        grabMarker.SetActive(true);
+        agent.isStopped = true;
+        yield return new WaitForSeconds(3f);
+        
+        //damage here
+        
+        grabbing = false;
+        grabMarker.SetActive(false);
+        agent.isStopped = false; 
+        animator.SetBool("IsWalking", true);
+        
+        spawner.AddLocation(currentTarget, currentListIndex);
+        MoveToPoint();
+      
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Exit"))
+        {
+            Destroy(gameObject);
+            spawner.npcCount--;
+            
+        }
+
+        if (other.CompareTag("Up"))
+        {
+            podiumDir.y = 1;
+            podiumDir.x = 0;
+           
+        }
+        if (other.CompareTag("Down"))
+        {
+            podiumDir.y = -1;
+            podiumDir.x = 0;
+         
+        }
+        if (other.CompareTag("Left"))
+        {
+            podiumDir.y = 0;
+            podiumDir.x = -1;
+       
+        }
+        if (other.CompareTag("Right"))
+        {
+            podiumDir.y = 0;
+            podiumDir.x = 1;
+     
+        }
+        
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Up"))
+        {
+            podiumDir.y = 0;
+            podiumDir.x = 0;
+           
+        }
+        if (other.CompareTag("Down"))
+        {
+            podiumDir.y = 0;
+            podiumDir.x = 0;
+         
+        }
+        if (other.CompareTag("Left"))
+        {
+            podiumDir.y = 0;
+            podiumDir.x = 0;
+       
+        }
+        if (other.CompareTag("Right"))
+        {
+            podiumDir.y = 0;
+            podiumDir.x = 0;
+     
+        }
+    }
+
+    void UpdateAnimator()
+    {
+        Vector3 velocity = agent.velocity;
+        direction = new Vector2(velocity.x, velocity.y).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (angle > -45 && angle <= 45)
+        {
+          
+            direction = Vector2.right;
+            spriteRenderer.flipX = false;
+        }
+        else if (angle > 45 && angle <= 135)
+        {
+         
+            direction = Vector2.up;
+            spriteRenderer.flipX = false;
+        }
+        else if (angle > 135 || angle <= -135)
+        {
+           
+            direction = Vector2.left;
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            direction = Vector2.down;
+            spriteRenderer.flipX = false;
+        }
+
+        //lastDirection = direction;
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+    }
+
+    void SortLayers()
+    {
+        float minY = -16f;  
+        float maxY = 10f;  
+     
+        int sortingOrder = (int)Mathf.Lerp(20, 3, Mathf.InverseLerp(minY, maxY, transform.position.y));
+
+   
+        spriteRenderer.sortingOrder = sortingOrder;
+        
+    }
+
+  
+
+
+}
---
+++ Assets/Ash/Scripts/Spawner.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Spawner : MonoBehaviour
+{
+    //todo
+    
+    // ***Get prefab
+    // ***Get spawn point
+    // ***Instantiate prefab at spawn point
+    // ***Spawn at set/random intervals
+    // ***Have a list of locations npc can walk to
+    // ***Randomly select a location from the list
+    // ***Set the target location of npc to the selected location
+    // ***npc walks to the location
+    // ***When the npc reaches location, remove location from list so another npc cant walk to the same location
+    // ***Set a timer for the npc to wait at the location
+    // ***When timer is up, add location back to list
+    // ***When npc has visited all 4 locations, leave the scene
+    
+    //todo
+    // ***fix the npc walking to the same location
+    // ***some locations are not being added to the list because they have been removed already
+    
+    
+    [SerializeField] GameObject npcPrefab;
+    public Transform spawnPoint;
+    public float spawnInterval;
+    public int npcMax = 10;
+    public Transform exitLocation;
+
+    public List<Transform> location1 = new List<Transform>();
+    public List<Transform> location2 = new List<Transform>();
+    public List<Transform> location3 = new List<Transform>();
+    public List<Transform> location4 = new List<Transform>();
+
+    private List<List<Transform>> allLocations = new List<List<Transform>>();
+    public int npcCount = 0;
+
+    private void Start()
+    {
+        allLocations.Add(location1);
+        allLocations.Add(location2);
+        allLocations.Add(location3);
+        allLocations.Add(location4);
+
+        StartCoroutine(SpawnNPC());
+    }
+
+    private IEnumerator SpawnNPC()
+    {
+        while (npcCount < npcMax)
+        {
+            Instantiate(npcPrefab, spawnPoint.position, Quaternion.identity);
+            npcCount++;
+            spawnInterval = UnityEngine.Random.Range(2f, 6f);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+        
+        spawnInterval = UnityEngine.Random.Range(2f, 3f);
+        yield return new WaitForSeconds(spawnInterval);
+        StartCoroutine(SpawnNPC());
+    }
+
+    public List<List<Transform>> GetAllLocations()
+    {
+        return allLocations;
+    }
+
+    public void AddLocation(Transform location, int listIndex)
+    {
+        allLocations[listIndex].Add(location);
+    }
+
+    public void RemoveLocation(Transform location, int listIndex)
+    {
+        allLocations[listIndex].Remove(location);
+    }
+    
+    
+}
